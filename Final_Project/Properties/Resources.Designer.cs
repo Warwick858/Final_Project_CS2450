ce798413fa7293c395bd3c5ae3828d49bb11cacb
James LoForti <jamesloforti@gmail.com>
2016-03-17 19:37:42 -0600

    Commit right before merge with Zack

--- Final_Project/Properties/Resources.Designer.cs
+++ Final_Project/Properties/Resources.Designer.cs
@@ -73,9 +73,9 @@ namespace Final_Project.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap background {
+        internal static System.Drawing.Bitmap backgroundlow {
             get {
-                object obj = ResourceManager.GetObject("background", resourceCulture);
+                object obj = ResourceManager.GetObject("backgroundlow", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
